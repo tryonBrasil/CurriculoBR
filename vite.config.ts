e361398341126f4bca90cdcc7f596b1cc8517f20
tryonBrasil <77490216+tryonBrasil@users.noreply.github.com>
2026-02-09 02:12:55 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -17,6 +18,9 @@ export default defineConfig(({ mode }) => {
     },
     build: {
       outDir: 'dist',
+      rollupOptions: {
+        external: ['pdfjs-dist']
+      }
     }
   }
 })
