8e3c0d684e9c4dcd66ac7286a29caa2b97e4ae63
tryonBrasil <77490216+tryonBrasil@users.noreply.github.com>
2026-02-12 22:46:21 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,12 @@
-
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // Carrega as variáveis de ambiente baseadas no modo (ex: .env)
-  // O cast (process as any) evita erros de tipagem caso @types/node não esteja perfeito
-  const env = loadEnv(mode, (process as any).cwd(), '');
-  
-  return {
-    plugins: [react()],
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"), // Isso diz que @ é a pasta raiz
     },
-    build: {
-      outDir: 'dist',
-      rollupOptions: {
-        output: {
-          manualChunks: {
-             pdfjs: ['pdfjs-dist']
-          }
-        }
-      }
-    }
-  }
+  },
 })
